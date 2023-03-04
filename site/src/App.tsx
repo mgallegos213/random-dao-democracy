48f9527fe5d527d167871baa62e818e527ca74e2
coderrick <derrick.duncan@temple.edu>
2023-03-04 14:12:40 -0500

    ui updates

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -3,26 +3,34 @@ import logo from './logo.svg';
 import './App.css';
 import Avatar from '@mui/material/Avatar';
 import Chip from '@mui/material/Chip';
+import Button from '@mui/material/Button';
 
 function App() {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+
   return (
     <main className="App">
-      
-      
-      <header>
-        <div style={{fontSize: "34px"}}>DAOMOCRACY</div>
-        <div>
-        <Chip avatar={<Avatar>M</Avatar>} label="Avatar" />
-      
-      </div></header>
+
+
+      <header style={{display: "flow-root"}}>
+        <span style={{
+          fontSize: "34px", float: "left",
+          margin: "25px 0px 0px 25px"
+        }}><strong>DAO</strong>MOCRACY</span>
+
+        <Chip style={{
+           float: "right",
+          margin: "25px 25px 25px 0px"
+        }} avatar={<Avatar>U</Avatar>} label="0x065BD7...D9AB" />
+
+      </header>
+
+      <div>
+      <Button style={{height: 200, width: 200}} variant="outlined">Vote</Button>
+      <Button style={{height: 200, width: 200}} variant="outlined">Discuss</Button>
+      <Button style={{height: 200, width: 200}} variant="outlined">Initiate</Button>
+      <Button style={{height: 200, width: 200}} variant="outlined">Review</Button>
+      </div>
     </main>
   );
 }
